cb8470cadc946c58af14059294cb1d173f819c7d
Nicolas-Z-C <nicolascampus117@gmail.com>
2025-12-04 11:03:02 -0500

    Feat :sparkles: Done whit the start of the dinamics

---
+++ JS_holamundo/src/04-objetos/01-intro.js
@@ -0,0 +1,18 @@
+let  user = {
+    name:'Nicolas',
+    email:'Nicolas@gmail.com',
+    direccion:{
+        calle:45,
+        carrera:50,
+    },
+    activo:true,
+    Recuperarclave:function(){ //Esto es una funcion anonima 
+        console.log("FF");
+    },
+}
+
+/* Este tipo de comportamiento, en el cual un objeto
+es el centro o el pivote de la programacion es conocido
+como POO o Programacion orientada a objetos, siempre que
+encontremos un grupo de datos, comportamientos o cualquier
+cosa parecida entre si estaremos viendo POO */ 
---
+++ JS_holamundo/src/04-objetos/02-dinamico.js
@@ -0,0 +1,26 @@
+const usr = {id:1};
+
+usr.name = 'Nicolas';
+usr.guardar = function (){
+    console.log("guardando",usr.guardar);
+};
+
+usr.guardar()
+
+//usr = 1; esto si genera un error
+
+/*A pesar de ser una constante, al ser un objeto o array, este ultimo si "Puede" cambiar, lo unico que no
+es el hecho de ser un objeto o array*/ 
+
+//Una forma de evitar agregar o modificar un objeto es con el siguiente metodo
+
+const usr1 = Object.freeze({id:1});
+usr1.name="Nico";
+console.log(usr1);
+
+//Si queremos agregar propiedades pero no poder modificarlas podemos usar este otro
+
+const usr2 = Object.seal({id:1});
+usr2.name="Nico";
+usr2.id=3;
+console.log(usr2); 
